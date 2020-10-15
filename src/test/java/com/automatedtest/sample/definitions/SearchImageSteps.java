2ba8687d0a9b3179610943655cf9766e054ce3ce
STI2K\vy.tue.nguyen <vy.tue.nguyen@logigear.com>
2020-10-15 09:20:40 +0700

    update scenario outline

--- src/test/java/com/automatedtest/sample/definitions/SearchImageSteps.java
+++ src/test/java/com/automatedtest/sample/definitions/SearchImageSteps.java
@@ -1,22 +1,26 @@
 package com.automatedtest.sample.definitions;
 
+import com.automatedtest.sample.pages.HomePage;
 import com.automatedtest.sample.pages.SearchResultPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 
 public class SearchImageSteps {
     private final SearchResultPage searchResultPage;
+    private final HomePage homePage;
 
     public SearchImageSteps() {
         this.searchResultPage = new SearchResultPage();
+        this.homePage = new HomePage();
     }
 
-    @And("I change to image tab")
-    public void aUserChangeToImageTab() {
+    @When("I search image for {string}")
+    public void aUserSearchImageWithString(String searchValue) {
+        this.homePage.searchFor(String.format("\"%s\"", searchValue));
         this.searchResultPage.changeToImageTab();
     }
 
-    @When("I drag and drop first image result to search image")
+    @And("I drag and drop first image result to search image")
     public void aUserDragAndDropFirstImageResultToSearchImage() throws Exception {
         this.searchResultPage.dragNDropFirstImageToSearch();
     }
--- src/test/resources/com/automatedtest/sample/ImageSearchByDropLink.feature
+++ src/test/resources/com/automatedtest/sample/ImageSearchByDropLink.feature
@@ -3,11 +3,9 @@ Feature: SearchImageByDropLink
   @searchImageByDropLink @searchImageByDropLink_01
   Scenario Outline: Verify Google Image search By Drop Link
     Given I'm on Google home page
-    And I search for "<queryText>"
-    And I change to image tab
-    When I drag and drop first image result to search image
+    When I search image for "<queryText>"
+    And I drag and drop first image result to search image
     Then Search result page should have image result
-
 
     Examples:
       | queryText    |
